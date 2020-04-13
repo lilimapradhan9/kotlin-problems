70f02738b1b19dcad552a06a6c3e7ce2b8d9b06a
Lilima Pradhan <lilima9@users.noreply.github.com>
2020-04-13 19:32:11 +0530

    Implement nesting of number

---
+++ src/main/kotlin/nestingDepth/Application.kt
@@ -0,0 +1,19 @@
+package nestingDepth
+
+import java.lang.System.`in`
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(`in`)
+    print("Enter number of inputs : ")
+    val numberOfInputs = scanner.nextInt()
+
+    for (index in 0 until numberOfInputs) {
+        print("Input : ")
+        val numberSequence = scanner.next()
+        val result = nestNumberWithParentheses(numberSequence)
+        print("Output : $result")
+        println()
+    }
+}
+
