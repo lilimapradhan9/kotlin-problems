b58d9e820a75375587c015b8075b8588b4cff359
Lilima Pradhan <24977719+lilimapradhan9@users.noreply.github.com>
2020-12-23 22:50:16 +0530

    Lilima | Solve using dp

--- src/main/kotlin/day2/longestPalindromeSubstring.kt
+++ src/main/kotlin/day2/longestPalindromeSubstring.kt
@@ -1,17 +1,23 @@
 package day2
 
-fun longestPalindrome(s: String): String {
-    var longestPalindrome = ""
+fun longestPalindrome(s: String): String? {
+    if (s.isEmpty()) return ""
 
-    for (i in s.indices) {
-        for (j in i until s.length) {
-            val substring = s.substring(i..j)
-            if (substring == substring.reversed()) {
-                if (substring.length > longestPalindrome.length) {
-                    longestPalindrome = substring
-                }
+    val inputLength = s.length
+    var start = 0
+    var end = 0
+
+    val memo = Array(inputLength) { BooleanArray(inputLength) }
+
+    for (i in inputLength - 1 downTo 0) {
+        for (j in i until inputLength) {
+            memo[i][j] = s[i] == s[j] && (j - i < 3 || memo[i + 1][j - 1])
+            if (memo[i][j] && j - i > end - start) {
+                start = i
+                end = j
             }
         }
     }
-    return longestPalindrome
+    return s.substring(start, end + 1)
 }
+
