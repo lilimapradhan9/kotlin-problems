882368abd90c82ebd6464f449e7415ce2ba9b724
Lilima Pradhan <24977719+lilimapradhan9@users.noreply.github.com>
2021-01-02 22:54:17 +0530

    Lilima | climb stairs

---
+++ src/test/kotlin/day9/ClimbStairsKtTest.kt
@@ -0,0 +1,27 @@
+package day9
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class ClimbStairsKtTest {
+    @Test
+    fun `should return the output for 1`() {
+        val stairs = climbStairs(1)
+
+        Assertions.assertEquals(1, stairs)
+    }
+
+    @Test
+    fun `should return the output for 2`() {
+        val stairs = climbStairs(2)
+
+        Assertions.assertEquals(2, stairs)
+    }
+
+    @Test
+    fun `should return the output for 3`() {
+        val stairs = climbStairs(3)
+
+        Assertions.assertEquals(3, stairs)
+    }
+}
