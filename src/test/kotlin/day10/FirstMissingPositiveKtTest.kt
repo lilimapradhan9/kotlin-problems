bdd2b8fe99f6fb1c11938159dc1286b9b3dc5cf7
Lilima Pradhan <24977719+lilimapradhan9@users.noreply.github.com>
2021-01-03 22:39:41 +0530

    Lilima | first missing positive number

---
+++ src/test/kotlin/day10/FirstMissingPositiveKtTest.kt
@@ -0,0 +1,27 @@
+package day10
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class FirstMissingPositiveKtTest {
+    @Test
+    fun `should return the smallest missing number for 1,2,0`() {
+        val number = firstMissingPositive(intArrayOf(1, 2, 0))
+
+        Assertions.assertEquals(3, number)
+    }
+
+    @Test
+    fun `should return the smallest missing number for 3,4,-1,1`() {
+        val number = firstMissingPositive(intArrayOf(3, 4, -1, 1))
+
+        Assertions.assertEquals(2, number)
+    }
+
+    @Test
+    fun `should return the smallest missing number for 7,8,9,11,12`() {
+        val number = firstMissingPositive(intArrayOf(7, 8, 9, 11, 12))
+
+        Assertions.assertEquals(1, number)
+    }
+}
