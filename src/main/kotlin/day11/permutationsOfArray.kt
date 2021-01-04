f7fd0cae93f627110ef427ba7426f3a7c47ff24d
Lilima Pradhan <24977719+lilimapradhan9@users.noreply.github.com>
2021-01-04 22:04:19 +0530

    Lilima | permutations of array

---
+++ src/main/kotlin/day11/permutationsOfArray.kt
@@ -0,0 +1,21 @@
+package day11
+
+fun permute(nums: IntArray): List<List<Int>> {
+    val result = mutableListOf<MutableList<Int>>()
+    val state = nums.indices.toMutableSet()
+
+    permute(nums, state, mutableListOf(), result)
+    return result
+}
+
+fun permute(nums: IntArray, state: Set<Int>, permutation: MutableList<Int>, result: MutableList<MutableList<Int>>) {
+    if (state.isEmpty()) {
+        result.add(permutation)
+        return
+    }
+    state.forEach {
+        val newState = state.toMutableSet()
+        newState.remove(it)
+        permute(nums, newState, (permutation + nums[it]).toMutableList(), result)
+    }
+}
---
+++ src/test/kotlin/day11/PermutationsOfArrayKtTest.kt
@@ -0,0 +1,18 @@
+package day11
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class PermutationsOfArrayKtTest {
+    @Test
+    fun `should return permutations for 1,2,3`() {
+        val permutations = permute(intArrayOf(1, 2, 3))
+
+        Assertions.assertEquals(listOf(1, 2, 3), permutations[0])
+        Assertions.assertEquals(listOf(1, 3, 2), permutations[1])
+        Assertions.assertEquals(listOf(2, 1, 3), permutations[2])
+        Assertions.assertEquals(listOf(2, 3, 1), permutations[3])
+        Assertions.assertEquals(listOf(3, 1, 2), permutations[4])
+        Assertions.assertEquals(listOf(3, 2, 1), permutations[5])
+    }
+}
