be26e137742d4a8fcbcfefddaf41c09b4bdaacbe
Lilima Pradhan <24977719+lilimapradhan9@users.noreply.github.com>
2021-01-05 22:13:08 +0530

    Lilima | rotate matrix

---
+++ src/main/kotlin/day12/rotateMatrix.kt
@@ -0,0 +1,27 @@
+package day12
+
+fun rotate(matrix: Array<IntArray>): Array<IntArray> {
+    transpose(matrix.size, matrix)
+    reverse(matrix.size, matrix)
+    return matrix
+}
+
+private fun reverse(n: Int, matrix: Array<IntArray>) {
+    for (i in 0 until n) {
+        for (j in 0 until n / 2) {
+            val tmp = matrix[i][j]
+            matrix[i][j] = matrix[i][n - j - 1]
+            matrix[i][n - j - 1] = tmp
+        }
+    }
+}
+
+private fun transpose(n: Int, matrix: Array<IntArray>) {
+    for (i in 0 until n) {
+        for (j in i until n) {
+            val tmp = matrix[j][i]
+            matrix[j][i] = matrix[i][j]
+            matrix[i][j] = tmp
+        }
+    }
+}
