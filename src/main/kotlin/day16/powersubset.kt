e70bff97ca10d1a05994a088a11f5bdaeede409d
Lilima Pradhan <24977719+lilimapradhan9@users.noreply.github.com>
2021-01-12 19:41:55 +0530

    Lilima | power subset

---
+++ src/main/kotlin/day16/powersubset.kt
@@ -0,0 +1,11 @@
+package day16
+
+fun subsets(nums: IntArray): List<List<Int>> {
+    val result = mutableListOf(emptyList<Int>())
+    nums.forEach { num ->
+        result.addAll(result.map { it + num })
+    }
+    return result
+}
+
+
