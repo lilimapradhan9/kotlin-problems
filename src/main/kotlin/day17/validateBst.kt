5c4d74bf827226d8b92afb1552c22f126275eb8a
Lilima Pradhan <24977719+lilimapradhan9@users.noreply.github.com>
2021-01-13 21:02:13 +0530

    Lilima | validate bst

---
+++ src/main/kotlin/day17/validateBst.kt
@@ -0,0 +1,18 @@
+package day17
+
+import models.TreeNode
+
+fun isValidBST(root: TreeNode?): Boolean {
+    return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE)
+}
+
+private fun isValidBST(root: TreeNode?, min: Long, max: Long): Boolean {
+    if (root == null)
+        return true
+
+    if (root.`val` >= max || root.`val` <= min)
+        return false
+
+    return isValidBST(root.left, min, root.`val`.toLong()) &&
+            isValidBST(root.right, root.`val`.toLong(), max)
+}
