047ee5fc42bcb0797ca9b1b08c4106959fdffa13
Lilima Pradhan <24977719+lilimapradhan9@users.noreply.github.com>
2021-01-14 21:05:43 +0530

    Lilima | zig zaf traversal

---
+++ src/test/kotlin/day18/ZigZagOrderTraversalKtTest.kt
@@ -0,0 +1,20 @@
+package day18
+
+import models.TreeNode
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class ZigZagOrderTraversalKtTest {
+    @Test
+    fun `should return the level order solution for 3,9,20,null,null,15,7`() {
+        val root = TreeNode(3)
+        root.left = TreeNode(9)
+        root.right = TreeNode(20)
+        root.right?.left = TreeNode(15)
+        root.right?.right = TreeNode(7)
+
+        val inorderTraversal = zigzagLevelOrder(root)
+
+        Assertions.assertEquals(listOf(listOf(3), listOf(20, 9), listOf(15, 7)), inorderTraversal)
+    }
+}
