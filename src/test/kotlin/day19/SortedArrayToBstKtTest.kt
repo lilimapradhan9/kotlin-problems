0cf98cb784225eb931c21cb68172f33bffd2fabe
Lilima Pradhan <24977719+lilimapradhan9@users.noreply.github.com>
2021-01-15 19:24:20 +0530

    Lilima | convert sorted array to bst

---
+++ src/test/kotlin/day19/SortedArrayToBstKtTest.kt
@@ -0,0 +1,17 @@
+package day19
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class SortedArrayToBstKtTest {
+    @Test
+    fun `should create tree for -10,-3,0,5,9`() {
+        val bst = sortedArrayToBST(intArrayOf(-10, -3, 0, 5, 9))
+
+        Assertions.assertEquals(0, bst?.`val`)
+        Assertions.assertEquals(-3, bst?.left?.`val`)
+        Assertions.assertEquals(9, bst?.right?.`val`)
+        Assertions.assertEquals(-10, bst?.left?.left?.`val`)
+        Assertions.assertEquals(5, bst?.right?.left?.`val`)
+    }
+}
