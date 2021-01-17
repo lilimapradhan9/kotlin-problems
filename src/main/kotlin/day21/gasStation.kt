f29e268785398a1a595e88c46d69a82fd5c5dfd9
Lilima Pradhan <24977719+lilimapradhan9@users.noreply.github.com>
2021-01-17 13:33:15 +0530

    Lilima | gas station two pointer

--- src/main/kotlin/day21/gasStation.kt
+++ src/main/kotlin/day21/gasStation.kt
@@ -16,3 +16,20 @@ fun canCompleteCircuit(gas: IntArray, cost: IntArray): Int {
     }
     return if (sum >= 0) index else -1
 }
+
+fun canCompleteCircuitTwoPointer(gas: IntArray, cost: IntArray): Int {
+    var start = gas.size - 1
+    var end = 0
+    var tank = gas[start] - cost[start]
+
+    while (start != end) {
+        if (tank >= 0) {
+            tank += gas[end] - cost[end]
+            end++
+        } else {
+            start--
+            tank += gas[start] - cost[start]
+        }
+    }
+    return if (tank < 0) -1 else start
+}
--- src/test/kotlin/day21/GasStationKtTest.kt
+++ src/test/kotlin/day21/GasStationKtTest.kt
@@ -17,4 +17,18 @@ class GasStationKtTest {
 
         Assertions.assertEquals(-1, startingPoint)
     }
+
+    @Test
+    fun `should return the starting point for 1,2,3,4,5 stations two pointer`() {
+        val startingPoint = canCompleteCircuitTwoPointer(intArrayOf(1, 2, 3, 4, 5), intArrayOf(3, 4, 5, 1, 2))
+
+        Assertions.assertEquals(3, startingPoint)
+    }
+
+    @Test
+    fun `should return the starting point for 2,3,4 stations two pointer`() {
+        val startingPoint = canCompleteCircuitTwoPointer(intArrayOf(2, 3, 4), intArrayOf(3, 4, 3))
+
+        Assertions.assertEquals(-1, startingPoint)
+    }
 }
