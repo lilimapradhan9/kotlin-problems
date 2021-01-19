b8b370cd924cc37631bbccdfe624c5bce89477b1
Lilima Pradhan <24977719+lilimapradhan9@users.noreply.github.com>
2021-01-19 23:45:18 +0530

    Lilima | lru implementation

---
+++ src/main/kotlin/day23/lruCache.kt
@@ -0,0 +1,74 @@
+package day23
+
+class LRUCache(private val capacity: Int) {
+    private val cache = LinkedHashMap<Int, Int>(capacity)
+    fun get(key: Int): Int {
+        val value = cache.remove(key) ?: return -1
+        cache[key] = value
+        return value
+    }
+
+    fun put(key: Int, value: Int) {
+        cache.remove(key)
+        if (cache.size == capacity) cache.remove(cache.keys.first())
+        cache[key] = value
+    }
+}
+
+class LRUCacheLL(private val capacity: Int) {
+    private class Node(val key: Int, var value: Int) {
+        var prev = dummy
+        var next = dummy
+
+        constructor() : this(0, 0)
+
+        companion object {
+            val dummy = Node()
+        }
+    }
+
+    private val head = Node()
+    private val map = mutableMapOf<Int, Node>()
+
+    init {
+        head.next = head
+        head.prev = head
+    }
+
+    fun get(key: Int) = map[key]?.let {
+        remove(it)
+        add(it)
+        it.value
+    } ?: -1
+
+    fun put(key: Int, value: Int) {
+        map[key]?.let {
+            remove(it)
+            add(it)
+            it.value = value
+            return
+        }
+
+        if (map.size == capacity) {
+            val node = head.next
+            remove(node)
+            map.remove(node.key)
+        }
+
+        val node = Node(key, value)
+        add(node)
+        map[key] = node
+    }
+
+    private fun remove(node: Node) {
+        node.prev.next = node.next
+        node.next.prev = node.prev
+    }
+
+    private fun add(node: Node) {
+        node.prev = head.prev
+        node.next = head
+        head.prev.next = node
+        head.prev = node
+    }
+}
---
+++ src/test/kotlin/day23/LRUCacheTest.kt
@@ -0,0 +1,36 @@
+package day23
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class LRUCacheTest {
+    @Test
+    fun `should create lru cache`() {
+        val lruCache = LRUCache(2)
+
+        lruCache.put(1, 1)
+        lruCache.put(2, 2)
+        Assertions.assertEquals(1, lruCache.get(1))
+        lruCache.put(3, 3)
+        Assertions.assertEquals(-1, lruCache.get(2))
+        lruCache.put(4, 4)
+        Assertions.assertEquals(-1, lruCache.get(1))
+        Assertions.assertEquals(3, lruCache.get(3))
+        Assertions.assertEquals(4, lruCache.get(4))
+    }
+
+    @Test
+    fun `should create lru cache using dll`() {
+        val lruCache = LRUCacheLL(2)
+
+        lruCache.put(1, 1)
+        lruCache.put(2, 2)
+        Assertions.assertEquals(1, lruCache.get(1))
+        lruCache.put(3, 3)
+        Assertions.assertEquals(-1, lruCache.get(2))
+        lruCache.put(4, 4)
+        Assertions.assertEquals(-1, lruCache.get(1))
+        Assertions.assertEquals(3, lruCache.get(3))
+        Assertions.assertEquals(4, lruCache.get(4))
+    }
+}
