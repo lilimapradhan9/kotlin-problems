80f81022ca65ef4956be3d860b8e730eb75408fd
Lilima Pradhan <24977719+lilimapradhan9@users.noreply.github.com>
2021-01-21 22:46:44 +0530

    Lilima | number of 1 bits

---
+++ src/test/kotlin/day25/NumberOf1BitsKtTest.kt
@@ -0,0 +1,27 @@
+package day25
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class NumberOf1BitsKtTest {
+    @Test
+    fun `return number of 31 for 11111111111111111111111111111101`() {
+        val hammingWeight = hammingWeight(-3)
+
+        Assertions.assertEquals(31, hammingWeight)
+    }
+
+    @Test
+    fun `return number of 1 for 00000000000000000000000010000000`() {
+        val hammingWeight = hammingWeight(128)
+
+        Assertions.assertEquals(1, hammingWeight)
+    }
+
+    @Test
+    fun `return number of 3 for 00000000000000000000000000001011`() {
+        val hammingWeight = hammingWeight(11)
+
+        Assertions.assertEquals(3, hammingWeight)
+    }
+}
