bcd995c4aa0a6a2a3ba61c4cf6cb016c68043574
Lilima Pradhan <24977719+lilimapradhan9@users.noreply.github.com>
2021-01-27 18:14:27 +0530

    Lilima | largest number

---
+++ src/main/kotlin/day26/largestNumber.kt
@@ -0,0 +1,16 @@
+package day26
+
+fun largestNumber(nums: IntArray): String {
+    if (nums.isEmpty()) return ""
+    val stringRep = Array(nums.size) { "" }
+
+    nums.forEachIndexed { index, number ->
+        stringRep[index] = number.toString()
+    }
+
+    stringRep.sortWith(Comparator { a, b -> (b + a).compareTo(a + b) })
+
+    if(stringRep[0][0] == '0') return "0"
+
+    return stringRep.reduce{result, element ->  result + element}
+}
