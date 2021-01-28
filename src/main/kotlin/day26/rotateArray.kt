f749658b5b8447c8fc31d62325d15d223edb5b9b
Lilima Pradhan <24977719+lilimapradhan9@users.noreply.github.com>
2021-01-28 16:14:56 +0530

    Lilima | rotate array

---
+++ src/main/kotlin/day26/rotateArray.kt
@@ -0,0 +1,21 @@
+package day26
+
+fun rotate(nums: IntArray, k: Int) {
+    val rotation = k % nums.size
+    var count = 0
+    var start = 0
+
+    while (count < nums.size) {
+        var current = start
+        var prev = nums[start]
+        do {
+            val next = (current + rotation) % nums.size
+            val temp = nums[next]
+            nums[next] = prev
+            prev = temp
+            current = next
+            count++
+        } while (start != current)
+        start++
+    }
+}
---
+++ src/test/kotlin/day26/RotateArrayKtTest.kt
@@ -0,0 +1,24 @@
+package day26
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class RotateArrayKtTest {
+    @Test
+    fun `should rotate 1,2,3,4,5,6,7 for k=3`() {
+        val nums = intArrayOf(1, 2, 3, 4, 5, 6, 7)
+
+        rotate(nums, 3)
+
+        Assertions.assertEquals(intArrayOf(5, 6, 7, 1, 2, 3, 4).toList(), nums.toList())
+    }
+
+    @Test
+    fun `should rotate -1,-100,3,99 for k=2`() {
+        val nums = intArrayOf(-1, -100, 3, 99)
+
+        rotate(nums, 2)
+
+        Assertions.assertEquals(intArrayOf(3, 99, -1, -100).toList(), nums.toList())
+    }
+}
