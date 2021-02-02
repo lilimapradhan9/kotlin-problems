31bfbe6901c06f32b6b187e75ab973d62dd15d5a
Lilima Pradhan <24977719+lilimapradhan9@users.noreply.github.com>
2021-02-02 22:51:26 +0530

    Lilima | happy number

---
+++ src/test/kotlin/day27/HappyNumberKtTest.kt
@@ -0,0 +1,20 @@
+package day27
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class HappyNumberKtTest {
+    @Test
+    fun `should return true for 19`() {
+        val happy = isHappy(19)
+
+        Assertions.assertTrue(happy)
+    }
+
+    @Test
+    fun `should return true for 2`() {
+        val happy = isHappy(2)
+
+        Assertions.assertFalse(happy)
+    }
+}
