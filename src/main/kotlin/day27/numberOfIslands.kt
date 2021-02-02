6408f456c10a3315afe4429a40bab970cb27d023
Lilima Pradhan <24977719+lilimapradhan9@users.noreply.github.com>
2021-02-02 22:30:56 +0530

    Lilima | number of islands

---
+++ src/main/kotlin/day27/numberOfIslands.kt
@@ -0,0 +1,27 @@
+package day27
+
+var gridlength = 0
+var gridBreadth = 0
+
+fun numIslands(grid: Array<CharArray>): Int {
+    var count = 0
+    gridlength = grid.size
+    if (gridlength == 0) return 0
+    gridBreadth = grid[0].size
+    for (i in 0 until gridlength) {
+        for (j in 0 until gridBreadth) if (grid[i][j] == '1') {
+            dfs(grid, i, j)
+            ++count
+        }
+    }
+    return count
+}
+
+private fun dfs(grid: Array<CharArray>, i: Int, j: Int) {
+    if (i < 0 || j < 0 || i >= gridlength || j >= gridBreadth || grid[i][j] != '1') return
+    grid[i][j] = '0'
+    dfs(grid, i + 1, j)
+    dfs(grid, i - 1, j)
+    dfs(grid, i, j + 1)
+    dfs(grid, i, j - 1)
+}
